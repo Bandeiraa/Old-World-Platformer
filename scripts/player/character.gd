767bddc35c921400ffbf17b3c296c82576f5fc45
davi bandeira <fcodavibandeira@edu.unifor.br>
2022-01-28 21:06:46 -0300

    Initializing player and stats scripts

--- scripts/player/character.gd
+++ scripts/player/character.gd
@@ -1,2 +1,22 @@
 extends KinematicBody2D
 class_name Player
+
+onready var stats: Node = get_node("Stats")
+onready var animation: AnimationPlayer = get_node("Animation")
+
+func _physics_process(delta: float) -> void:
+	move()
+	stats.velocity = move_and_slide(stats.velocity)
+	animate()
+	
+	
+func move() -> void:
+	var input: float = Input.get_action_strength("right") - Input.get_action_strength("left")
+	stats.velocity.x = stats.speed * input
+	
+	
+func animate() -> void:
+	if stats.velocity.x != 0:
+		animation.play("walk")
+	else:
+		animation.play("idle")
---
+++ scripts/player/stats.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name PlayerStats
+
+var velocity: Vector2
+
+export(int) var speed
